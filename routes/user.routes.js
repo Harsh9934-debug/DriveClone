a1ceb36278b81a616cae290b319f184e35de952f
Harsh Kumar Gupta <harshkumargupta630@gmail.com>
2025-09-22 19:49:09 +0530

    Created the register routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/test', (req, res) => {
-    res.send('User test routes');
+// router.get('/test', (req, res) => {
+//     res.send('User test routes');
+// });
+
+
+router.get('/register', (req, res) => {
+    res.render('register');
 });
-
-
 
 module.exports = router;
