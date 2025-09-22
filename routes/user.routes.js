1aaa3caf6a6d9c651d956ea1fcbe8ae31a740df3
Harsh Kumar Gupta <harshkumargupta630@gmail.com>
2025-09-22 22:15:23 +0530

    Created the middleware to check weather this is correct or not

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');  // this is used for validation of form data
 
 // router.get('/test', (req, res) => {
 //     res.send('User test routes');
@@ -10,7 +11,18 @@ router.get('/register', (req, res) => {
     res.render('register');
 });
 
-router.post('/register', (req, res) => {
+router.post('/register', 
+    body('email').trim().isEmail(),
+    body('password').trim().isLength({ min: 4 }),
+    body('name').trim().isLength({ min: 3 }),
+    (req, res) => {
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {        
+            return res.status(400).json({ errors: errors.array(), message: 'Invalid data' });
+        }
+        res.send(errors)
+
     console.log(req.body); // You can see the form data in the console by using this 
     res.send('User registered successfully!');
 });
